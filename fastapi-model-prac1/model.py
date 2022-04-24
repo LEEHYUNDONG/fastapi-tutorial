85c469a813ce627c47d9e739fbe1359a22bba0e5
LEEHYUNDONG <l.steve10@yahoo.com>
2022-04-24 21:03:40 +0900

    user model using base

---
+++ fastapi-model-prac1/model.py
@@ -0,0 +1,26 @@
+from enum import auto
+import typing
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(30))
+    # fullname = Column(String(30))
+    password = Column(String(200))
+
+
+
+    # user = User(id="leerfer@naver.com", password="1234")
+    # print(user)
+
+    # login
+    # https://fastapi.tiangolo.com/tutorial/security/
+
+    # oauth + passlib
+    # https://fastapi.tiangolo.com/tutorial/security/oauth2-jwt/
+
+    # database
+    # https://fastapi.tiangolo.com/tutorial/sql-databases/
