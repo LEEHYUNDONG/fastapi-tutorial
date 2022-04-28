556628503bf206761cf7da1267bc1492ff2f1ef2
LEEHYUNDONG <l.steve10@yahoo.com>
2022-04-28 16:14:29 +0900

    fake db

---
+++ fastapi-login/DB.py
@@ -0,0 +1,17 @@
+DB = {
+    "users":{
+        "hyundong":{
+            "username":"hyundong",
+            "age" : 14,
+            "fullname" : "leehyundong",
+            "hashed_password" : "$2b$12$AWhHnUDIUsqBLJi.LWaOtucNmGVP9hR63YWOsfOMh8lHNVFu1oq6K"
+        },
+        "yc":{
+            "username":"youngchan",
+            "age" : 10,
+            "fullname" : "RohYoungChan",
+            "hashed_password" : "$2b$12$laHNzlvsit/kEZ39uW0buOX.EnEVX1.69mqtNdt/8RNX36mgdMJ2O"
+        }
+        
+    }
+}
